838eb15e07f4189815e9f98367b17f8003dc8f98
MightyManrock <MightyManrock@gmail.com>
2023-08-16 01:38:13 -0400

    Added support for VI keys

--- input_handlers.py
+++ input_handlers.py
@@ -21,11 +21,12 @@ if TYPE_CHECKING:
   from entity import Item
 
 MOVE_KEYS = {
+  # Arrow keys
   tcod.event.KeySym.UP: (0, -1),
   tcod.event.KeySym.DOWN: (0, 1),
   tcod.event.KeySym.LEFT: (-1, 0),
   tcod.event.KeySym.RIGHT: (1, 0),
-
+  # Numbpad keys
   tcod.event.KeySym.KP_1: (-1, 1),
   tcod.event.KeySym.KP_2: (0, 1),
   tcod.event.KeySym.KP_3: (1, 1),
@@ -34,6 +35,15 @@ MOVE_KEYS = {
   tcod.event.KeySym.KP_7: (-1, -1),
   tcod.event.KeySym.KP_8: (0, -1),
   tcod.event.KeySym.KP_9: (1, -1)
+  # VI keys
+  tcod.event.KeySym.h: (-1, 0),
+  tcod.event.KeySym.j: (0, 1),
+  tcod.event.KeySym.k: (0, -1),
+  tcod.event.KeySym.l: (1, 0),
+  tcod.event.KeySym.y: (-1, -1),
+  tcod.event.KeySym.u: (1, -1),
+  tcod.event.KeySym.b: (-1, 1),
+  tcod.event.KeySym.n: (1, 1)
 }
 
 WAIT_KEYS = {
